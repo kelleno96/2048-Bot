e97773b80f070cea7f117528a1bb468ecc575ddc
kelleno96 <kellen.oconnor01@gmail.com>
2018-12-26 17:18:09 -0600

    Complete game, add AI

--- 2048.py
+++ 2048.py
@@ -5,14 +5,37 @@ class environment:
     def __init__(self):
         print("Making game")
         self.board = np.zeros([4,4], dtype=np.uint)
-        self.lock = np.zeros([4,4], dtype=np.uint)
+        self.add_random_tile()
+
     def __str__(self):
         return str(self.board) + "\n"
 
+    def move(self, action):
+        # action = 0 for left, 1 up, 2 right, 3 down
+        rotated_board = np.rot90(self.board, action)
+        cols = [rotated_board[i, :] for i in range(4)]
+        new_board, rewards = zip(*np.array([self.move_left(col) for col in cols]))
+        reward = np.sum(rewards)
+        new_board = np.array(new_board)
+        # print("New board: " + str(new_board))
+        # print(np.rot90(new_board, -action))
+        return np.rot90(new_board, -action), reward
+
+    def is_done(self):
+        return all([(self.move(a)[0]==self.board).all() for a in range(4)])
+
     def step(self, action):
-        # Perform action
-        # Return next board state (Sprime), Reward, Done
-        pass
+        # Return new board state, reward for the step, isDone
+        done = self.is_done()
+        if not done:
+            new_board, reward = self.move(action)
+            if not (new_board == self.board).all():
+                self.board = new_board.copy()
+                self.add_random_tile()
+        else:
+            reward = 0
+        return self.board, reward, done
+
 
     """
     Merging:
@@ -21,40 +44,39 @@ class environment:
     
     
     """
-    def move_up(self):
-        # Slide tiles up
-        for col in range(0, 4):
-            for row in range(1, 4):
-                for i in range(4-row):
-                    if self.board[row, col] == self.board[row+i, col] and np.sum(self.board[row+1:row+i-1, col]) == 0 and self.lock[row, col] == 0 and self.lock[row+i, col] == 0:
-                        self.lock[row, col] = 1
-                        self.lock[row+i, col] = 1
-                    # if self.board[row - i, col] == self.board[row, col] and np.sum(self.board[i:row, col]) == 0:
-                    #     self.board[row - 1, col] *= 2
-                    #     self.board[row, col] = 0
-                # if self.board[row, col]!= 0:
-                #     if self.board[row-1, col] == 0:
-                #         self.board[row-1, col] = self.board[row, col]
 
-
-        return self.board
-    def move_down(self):
-        # Slide tiles down
-        pass
-    def move_left(self):
+    def move_left(self, col):
         # Slide tiles left
-        pass
-    def move_right(self):
-        # Slide tiles right
-        pass
+        new_col = np.zeros((4), dtype=np.uint)
+        j = 0
+        previous = None
+        reward = 0
+        for i in range(col.size):
+            if col[i] != 0:
+                if previous == None:
+                    previous = col[i]
+                else:
+                    if previous == col[i]:
+                        new_col[j] = 2*col[i]
+                        reward += 2*col[i]
+                        j+=1
+                        previous = None
+                    else:
+                        new_col[j] = previous
+                        j+=1
+                        previous = col[i]
+        if previous!=None:
+            new_col[j] = previous
+        return new_col, reward
 
     def add_random_tile(self):
         row = np.random.randint(0, 4)
         col = np.random.randint(0, 4)
-        while(self.board[row, col] != 0):
-            row = np.random.randint(0, 4)
-            col = np.random.randint(0, 4)
-        self.board[row, col] = 2
+        if (self.board==0).any():
+            while(self.board[row, col] != 0):
+                row = np.random.randint(0, 4)
+                col = np.random.randint(0, 4)
+            self.board[row, col] = 2 + 2*np.random.randint(0,1)
 
     def print_board(self):
         print(self.board)
@@ -66,14 +88,35 @@ class environment:
 
 def main():
     env = environment()
-    env.board = np.array([[2, 2, 4, 8],
-                         [2, 0, 2, 4],
-                         [2, 2, 0, 2],
-                         [2, 2, 2, 0]], dtype=np.uint)
+    key = 'asdf'
+    score = 0
     print(env)
-    env.move_up()
-    print(env)
-    print(str(env.lock))
+    while key != 'q':
+        key = input("Direction: ")
+        if key == 'w':
+            action = 1
+        if key == 'd':
+            action = 2
+        if key == 's':
+            action = 3
+        if key == 'a':
+            action = 0
+        board, reward, done = env.step(action)
+        if(done):
+            print("You lose")
+            print(score)
+            break
+        score+=reward
+        print(board)
+        print(score)
+    # env.board = np.array([[2, 2, 4, 8],
+    #                      [2, 0, 2, 4],
+    #                      [2, 2, 0, 2],
+    #                      [2, 2, 2, 0]], dtype=np.uint)
+    # print(env)
+    # ret = env.step(1)
+    # print(env)
+    # print(ret)
 
 
 
